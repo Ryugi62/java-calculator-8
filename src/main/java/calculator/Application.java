735e2ed725679bc80507389725223794971343ac
= <=>
2025-10-17 05:34:37 +0900

    feat: 사용자 입력 처리 및 결과 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // text_input이라는 변수에 값 입력받음
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String text_input = sc.nextLine();
+
+        System.out.println("결과 : " + text_input);
+
+
+        // text_input에 ;이나 ,을 기준으로 문자를 각각 분리해서 배열에 넣는다.
+
+        // //으로 시작해서 \n으로 끝나는 문자를 각각 분리해서 배열에 넣는다.
+
+        // 아직 text_input에 구분자가 남아있으면 오류 return 후 종료
+
+        // 각 숫자들끼리 더한 후 결과값 출력
+        System.out.println(result);
     }
 }
