3e80fd83dc019294ceab9ea188103ba1f000c3b4
= <=>
2025-10-17 16:08:09 +0900

    feat: 입력 문자열 분리 정규 표현식 개선 및 오류 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,20 +12,22 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String text_input = sc.nextLine();
 
-        System.out.println("결과 : " + text_input);
-
-
         // text_input에 ;이나 ,을 기준으로 문자를 각각 분리해서 배열에 넣는다.
-        String[] arr = text_input.split(";|,");
+        // //으로 시작해서 \n으로 끝나는 문자를 각각 분리해서 배열에 넣는다.
+        String regex = "[;,]"; // ; 또는 , 또는 //로 시작해서 \n으로 끝나는 것들을 기준으로 분리한다는 정규표현식
+        System.out.println("regex: " + regex);
+
+        String[] arr = text_input.split(regex);
 
         for (int i = 0; i < arr.length; i++) {
-            // result += int(arr[i]);
+            // 아직 text_input에 구분자가 남아있으면 오류 return 후 종료
+            if (Pattern.matches("[0-9]", arr[i]))
+                throw new ExceptionInInitializerError();
+
             result += Integer.parseInt(arr[i]);
+            System.out.println("index[" + i + "]: " + arr[i]);
         }
 
-        // //으로 시작해서 \n으로 끝나는 문자를 각각 분리해서 배열에 넣는다.
-
-        // 아직 text_input에 구분자가 남아있으면 오류 return 후 종료
 
         // 각 숫자들끼리 더한 후 결과값 출력
         System.out.println(result);
