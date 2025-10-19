6e75f2e4a789acf471590f318395d22379269754
= <=>
2025-10-19 19:47:24 +0900

    fix: 가독성을 위한 주석 수정 및 결과 출력 형식 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -18,14 +18,11 @@ public class Application {
         // //으로 시작해서 \n으로 끝나는 문자를 각각 분리해서 배열에 넣는다.
         // ; 또는 , 또는 //로 시작해서 \n으로 끝나는 것들을 기준으로 분리한다는 정규표현식
         Pattern pattern = Pattern.compile(",|;|//.*\\\\n");
-        System.out.println("regex: " + pattern);
 
         String[] arr = pattern.split(text_input);
 
         for (int i = 0; i < arr.length; i++) {
-            // 우선 가독성을 위해서 arr[i]를 출력.
-            System.out.println("index[" + i + "]: " + arr[i]);
-            
+            // arr[i]가 비워져있는 경우 다음으로 건너뜀
             if (arr[i] == "") continue;
 
             // arr[i]가 숫자가 아니면 오류를 throw. 
@@ -34,10 +31,11 @@ public class Application {
                 throw new IllegalArgumentException();
             }
 
+            // 이상없는 arr[i] result에 더함
             result += Integer.parseInt(arr[i]);
         }
  
         // 각 숫자들끼리 더한 후 결과값 출력
-        System.out.println("결과" + result);
+        System.out.println("결과 : " + result);
     }
 }
